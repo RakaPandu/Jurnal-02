1be3a0650f7af7448768ffec6e75f2988c69cbf7
Your Name <you@example.com>
2021-09-27 16:37:33 +0700

    first commit

---
+++ Test2.java
@@ -0,0 +1,8 @@
+public class Test2 {
+
+	public static void main(String[] args) {
+		System.out.println("Nama	: Raka Pandu Aulia") ;
+		System.out.println("NIM  	: 6706213068") ;
+		System.out.println("Kelas	: D3RPLA-45-03") ;
+	}
+}
